ebc659c338cbcf12423102b24afe851bc8abd97a
MANJARREX <manjarresoswaldo338@gmail.com>
2024-05-28 11:19:07 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -6,13 +6,41 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
     const password = document.getElementById('password').value;
     const message = document.getElementById('message');
 
-    // Aquí puedes añadir la lógica para validar el usuario y la contraseña
-    if (role === 'inventario' || role === 'vendedor' || role === 'contador') {
-        // Redirigir a la página de menú
-        window.location.href = 'menu.html';
+    // Aquí  el usuario y la contraseña
+    if (role === 'inventario') {
+        // Redirigir a la página de inventario
+        window.location.href = 'inventario.html';
+    } else if (role === 'vendedor') {
+        // Redirigir a la página de vendedor
+        window.location.href = 'vendedor.html';
+    } else if (role === 'contador') {
+        // Redirigir a la página de contador 
+        message.style.color = 'red';
+        message.textContent = 'Funcionalidad para contador en desarrollo.';
     } else {
         // Mostrar un mensaje de error si no se selecciona un rol válido
         message.style.color = 'red';
         message.textContent = 'Seleccione un rol válido.';
     }
+});
+
+// Función para mostrar el mensaje "EN PROCESO..."
+function showProcessingMessage() {
+    var processingMessage = document.getElementById('processing-message');
+    processingMessage.style.display = 'block';
+    setTimeout(function(){
+        processingMessage.style.display = 'none';
+    }, 2000); 
+}
+
+// Función para mostrar el mensaje "REVISANDO STOCK..."
+function showStockMessage() {
+    var stockMessage = document.getElementById('stock-message');
+    stockMessage.style.display = 'block';
+    setTimeout(function(){
+        stockMessage.style.display = 'none';
+    }, 1500); 
+}
+document.getElementById("revisar-stock-btn").addEventListener("click", function(event) {
+    window.location.href = "stock.html";
 });
