69d8f84ef1a1e3bcc464483645f7fb1f7131c831
MANJARREX <manjarresoswaldo338@gmail.com>
2024-05-28 00:03:34 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -7,9 +7,16 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
     const message = document.getElementById('message');
 
     // Aquí puedes añadir la lógica para validar el usuario y la contraseña
-    if (role === 'inventario' || role === 'vendedor' || role === 'contador') {
-        // Redirigir a la página de menú
-        window.location.href = 'menu.html';
+    if (role === 'inventario') {
+        // Redirigir a la página de inventario
+        window.location.href = 'inventario.html';
+    } else if (role === 'vendedor') {
+        // Redirigir a la página de vendedor
+        window.location.href = 'vendedor.html';
+    } else if (role === 'contador') {
+        // Redirigir a la página de contador (aquí puedes agregar la lógica correspondiente)
+        message.style.color = 'red';
+        message.textContent = 'Funcionalidad para contador en desarrollo.';
     } else {
         // Mostrar un mensaje de error si no se selecciona un rol válido
         message.style.color = 'red';
@@ -17,12 +24,20 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
     }
 });
 
-// Función para mostrar el mensaje "EN PROCESO..." y redirigir después de un tiempo
-function showProcessingMessage(targetPage) {
+// Función para mostrar el mensaje "EN PROCESO..."
+function showProcessingMessage() {
     var processingMessage = document.getElementById('processing-message');
     processingMessage.style.display = 'block';
     setTimeout(function(){
         processingMessage.style.display = 'none';
-        window.location.href = targetPage;
     }, 2000); // El mensaje se ocultará automáticamente después de 2 segundos (2000 milisegundos)
+}
+
+// Función para mostrar el mensaje "REVISANDO STOCK..."
+function showStockMessage() {
+    var stockMessage = document.getElementById('stock-message');
+    stockMessage.style.display = 'block';
+    setTimeout(function(){
+        stockMessage.style.display = 'none';
+    }, 1500); // El mensaje se ocultará automáticamente después de 1.5 segundos (1500 milisegundos)
 }
